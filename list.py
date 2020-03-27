0480ba88ba745777962152650ca31712bf76e40e
mrUnknown <chambharem7@gmail.com>
2020-03-28 00:51:33 +0530

    py

---
+++ list.py
@@ -0,0 +1,7 @@
+#*************************List*****************#
+
+fruit = ["apple","mango","banana","orange"]
+
+print(fruit[0])    #index
+print(fruit[0:])
+print(fruit[:-1])
---
+++ string Data type.py
@@ -0,0 +1,47 @@
+#****************************************Data types *******************************************#
+# 01) String
+
+
+#----------------String-----------------@
+"""print()
+print("-------------------------------------------String------------------------------------------")
+print()
+a = "hello" #(string)
+print(a) #(print statement)
+print(type(a)) #(type)
+print()"""
+
+""" ------------Output------------
+
+hello
+<class 'str'>
+
+-----------------------------------"""
+
+#============================Indexing====================================#
+
+"""print()
+print("-------------------------------------------String with indexing------------------------------------------")
+print()
+a = "hello World" #(string)
+b = a[0:4]
+print(b) #(print statement + Indexing)
+print(type(a)) #(type)
+print()"""
+
+""" ------------Output------------
+hell
+<class 'str'>
+
+-----------------------------------"""
+
+
+#practice
+
+
+print()
+a = "hello World" 
+b = a[0]
+print(b) 
+print(type(a)) #(type)
+print()
